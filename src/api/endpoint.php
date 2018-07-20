a6bb4bbb58405ae9f38dfa4d02ea46dfcc541f0e
Connor <c.mcg132@gmail.com>
2018-07-20 10:25:26 -0400

    Updated test endpoint to match readme

--- src/api/endpoint.php
+++ src/api/endpoint.php
@@ -5,35 +5,16 @@
     class Endpoint extends RESTfulEndpoint {
 
         function get($params) {
-
-            $this->db->verify_table('comments');
-
-            $response = [];
-
-            $response['test'] = 'works';
-
-            return $response;
+            
+            if (!array_key_exists('id', $params)) {
+                return ['error' => 'You need to enter an ID'];
+            }
+            
+            return ['message' => 'Success'];
         }
 
         function post($params) {
-
-            $this->db->verify_table('comments');
-
-            $name = trim($params['name']);
-            $content = trim($params['content']);
-
-            if ($name === '' || $content === '') {
-                return ['error' => 'You must enter a title, body, and tags'];
-            }
-
-            $message = $this->db->insert('comments', [
-                'name' => $name,
-                'content' => $content,
-                'test' => 3
-            ]) ? 'success' : 'error';
-
-            return ['message' => $message];
-
+            return ['message' => 'POST method works!'];
         }
 
     }
