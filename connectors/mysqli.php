de437f6b23feccaf3cc93b6963414ad70c4669bc
Connor <c.mcg132@gmail.com>
2018-07-21 09:08:02 -0400

    Added DatabaseResult class

--- connectors/mysqli.php
+++ connectors/mysqli.php
@@ -15,7 +15,7 @@
         }
 
         function query($sql) {
-            return $this->connection->query($sql);
+            return new MySQLiResult($this->connection->query($sql));
         }
 
         function insert_id() {
@@ -29,6 +29,38 @@
         function disconnect() {
             return $this->connection->close();
         }
+    }
+
+    class MySQLiResult extends DatabaseResult {
+
+        function __construct($result) {
+            $this->result = $result;
+        }
+
+        function set_current_row($index) {
+            return $this->result->data_seek($index);
+        }
+
+        function get_next_row($enumerated=false) {
+            return $enumerated ? $this->result->fetch_row() : $this->result->fetch_assoc();
+        }
+
+        function get_all_rows($enumerated=false) {
+            return $enumerated ? $this->result->fetch_all() : $this->result->fetch_all(MYSQLI_ASSOC);
+        }
+
+        function get_row_as_class($className, $params=null) {
+            return $this->result->fetch_object($className, $params);
+        }
+
+        function get_field_info() {
+            return $this->result->fetch_fields();
+        }
+
+        function free() {
+            return $this->result->free();
+        }
+
     }
     
 
--- database.php
+++ database.php
@@ -29,6 +29,36 @@
 
     }
 
+    class DatabaseResult {
+
+        // Sets the current row index
+        function set_current_row($index) {
+        }
+
+        // Returns an array of the next row
+        // Result keys will be indexed if $enumerated is true, otherwise the keys will be field names
+        function get_next_row($enumerated=false) {
+        }
+
+        // Returns an array of all rows
+        // Result keys will be indexed if $enumerated is true, otherwise the keys will be field names
+        function get_all_rows($enumerated=false) {
+        }
+
+        // Returns a class instance with values from the row set as properties
+        // keys in $params will also be added as properties 
+        function get_row_as_class($className, $params=null) {
+        }
+
+        // Returns an array of field info
+        function get_field_info() {
+        }
+
+        function free() {
+        }
+
+    }
+
     class DatabaseConnection {
 
         static $table_config;
